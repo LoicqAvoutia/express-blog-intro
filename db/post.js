ed02c8449fcb739e23e899c6a0ded96b3770ec23
LoicqAvoutia <avoutialoicqofficial@gmail.com>
2026-09-04 12:53:22 +0200

    primo commit

---
+++ db/post.js
@@ -0,0 +1,52 @@
+export const postdb = [
+    {
+        titolo: 'titolo1',
+        contenuto: 'contenuto1',
+        immagine: '../public/img/20760.jpg',
+        tags:[
+            'bello',
+            'tranquillo',
+            'splendido'
+        ]
+    },
+    {
+        titolo: 'titolo2',
+        contenuto: 'contenuto2',
+        immagine: '../public/img/20770.jpg',
+        tags:[
+            'bello',
+            'tranquillo',
+            'splendido'
+        ]
+    },
+    {
+        titolo: 'titolo3',
+        contenuto: 'contenuto3',
+        immagine: '../public/img/20775.jpg',
+        tags:[
+            'bello',
+            'tranquillo',
+            'splendido'
+        ]
+    },
+    {
+        titolo: 'titolo4',
+        contenuto: 'contenuto4',
+        immagine: '../public/img/20786.jpg',
+        tags:[
+            'bello',
+            'tranquillo',
+            'splendido'
+        ]
+    },
+    {
+        titolo: 'titolo5',
+        contenuto: 'contenuto5',
+        immagine: '../public/img/20813.jpg',
+        tags:[
+            'bello',
+            'tranquillo',
+            'splendido'
+        ]
+    }
+]
---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+Esercizio
+
+Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità sulla base di quello che impareremo. 
+
+Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
+Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+Testare su postman
+*/
+import express from "express";
+import { postdb } from "./db/post.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  return res.send('server del mio blog');
+});
+
+app.get("/bacheca", (req, res) => {
+  return res.json(postdb);
+});
+
+
+app.listen(PORT, () => {
+  console.log("Server avviato");
+});
+
